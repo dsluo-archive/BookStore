42c7ec7928c212f6e89d5a25d32eea875198171f
dsluo <dsluo@users.noreply.github.com>
2018-06-20 15:55:03 -0400

    Use static directory and DRYed css

---
+++ static_cdn/scripts/landing.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    console.log("I AM READING THE SCRIPT");
+    var popup = document.getElementById('myPopup');
+    var doLogin = document.getElementById("doLogin");
+
+    doLogin.onclick = function () {
+        "use strict";
+        clearValues();
+        currentSubmit = "#login";
+        popup.style.display = "block";
+    };
+}

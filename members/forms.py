6873769aee35d59698be79c5a517a35ea416527c
Brian-Burgess <brian@brian-burgess.me>
2018-07-09 23:59:01 -0400

    Requirement 4:
    + Made email be a unique field.

--- members/forms.py
+++ members/forms.py
@@ -30,6 +30,13 @@ class UserCreateForm(forms.ModelForm):
             "username": None,
         }
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError('Email addresses must be unique.')
+        return email
+
 
 class UserLoginForm(forms.Form):
     username = forms.CharField(max_length=30)

d98422a3491e5e203c1f658f93852d56d07ba473
Brian-Burgess <brian@brian-burgess.me>
2018-07-13 18:29:37 -0400

    Various changes to Cart, Order, CartItem, Address and Member Admin pages.

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
+from cart.models import Cart, Order, CartItem
+
+
 # Register your models here.
+class CartItemModelAdmin(admin.ModelAdmin):
+    list_display = ["book"]
+
+    class Meta:
+        model = CartItem
+
+
+class CartModelAdmin(admin.ModelAdmin):
+    list_display = ["member", "total"]
+
+    def member(self, obj):
+        return obj
+
+    class Meta:
+        model = Cart
+
+
+class OrderModelAdmin(admin.ModelAdmin):
+    list_display = ["cart", "confirmation_number", "date"]
+    search_fields = ["confirmation_number"]
+
+    class Meta:
+        model = Order
+
+admin.site.register(Cart, CartModelAdmin)
+admin.site.register(Order, OrderModelAdmin)
+admin.site.register(CartItem, CartItemModelAdmin)
+

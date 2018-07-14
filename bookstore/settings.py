03d7fe713cfc0c699fe093f71850c49bc65017df
dsluo <dsluo@users.noreply.github.com>
2018-07-13 22:00:00 -0400

    use static and DRY static files

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'bookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates/AJ')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

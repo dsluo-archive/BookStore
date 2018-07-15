08f65c6cab3244cf7152d23c019178c2cd7d234e
Brian-Burgess <brian@brian-burgess.me>
2018-07-14 22:57:26 -0400

    Removed unused import from books.urls

--- books/urls.py
+++ books/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import re_path
 
 from books import views as books_views
 
@@ -12,8 +12,5 @@ urlpatterns = [
     re_path(r'^book/create-book/$', books_views.create_book, name='create'),
     re_path(r'^book/mass-create-book/$', books_views.mass_create_book, name='mass_create'),
     re_path(r'^book/(?P<slug>[\w-]+)/$', books_views.detail, name='detail'),
-
-    re_path(r'^q/$', books_views.query, name='query'),
-
     # make / url pattern redirect to home/ if logged in
 ]

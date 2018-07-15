8611ec1d66e9a0b63c06aadab914f561fd372827
Brian-Burgess <brian@brian-burgess.me>
2018-07-14 21:37:45 -0400

    Cart App Additions:
    + Added braintree as an import, and configured.
    + Added a price field to CartItem
    + Changed name of add_purchase and remove_purchase to add_item and
      remove_item
    + Modified remove_item to remove the CartItem, not delete it.
    + Created a UserCheckout Model to handle braintree and checkouts
    + Created a update_braintree_id method to get braintree_id
    + Added an items (MtoM w/ CartItem), reservation (Boolean), and
      is_fulfilled (Boolean) field. Modified address_used to be a ForeignKey
      instead of OnetoOne.
    + Added a price property method to Order.
    + Added a cancel_order method called by celery task and deletes the
      order and sends a confirmation to the relevant user (currently only
      used with reservations)
    + Created an order_id_generator method to get unique id's for Orders
    + Created a post_save_receiver for Orders which sends an email with all
      relevant information to the user.

--- cart/models.py
+++ cart/models.py
@@ -1,17 +1,33 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.conf import settings
+from django.core.mail import send_mail
+import random, string
 
 # Create your models here.
 
 from books.models import Book
-from cart.tasks import cancel_reservation
+
+import braintree
+
+if settings.DEBUG:
+    gateway = braintree.BraintreeGateway(
+        braintree.Configuration(
+            environment=braintree.Environment.Sandbox,
+            merchant_id=settings.BRAINTREE_MERCHANT_ID,
+            public_key=settings.BRAINTREE_PUBLIC,
+            private_key=settings.BRAINTREE_PRIVATE
+        )
+    )
 
 
 class CartItem(models.Model):
-    reservation = models.BooleanField(default=False)
     date_added = models.DateTimeField(auto_now_add=True)
     count = models.IntegerField(default=1)
     book = models.ForeignKey(Book, null=False, blank=False, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2, blank=False)
 
     def __str__(self):
         return self.book.name
@@ -26,7 +42,7 @@ class Cart(models.Model):
                                 default=0.00,
                                 blank=True)
 
-    def add_purchase(self, member, book, count, reservation):
+    def add_item(self, member, book, count):
 
         cart_item = member.cart.items.all().filter(book=book).first()
 
@@ -34,30 +50,143 @@ class Cart(models.Model):
             cart_item.count = count
             cart_item.save()
         else:
-            new_item = CartItem(reservation=reservation, count=count, book=book)
+            new_item = CartItem(count=count, book=book, price=book.price)
             new_item.save()
-
-            if reservation:
-                cancel_reservation.apply_async(args=[member.slug, book.slug], countdown=432000)
 
             self.items.add(new_item)
 
     @staticmethod
-    def cancel_purchase(member, book):
-        cart_item = member.cart.items.all().filter(book=book)
+    def remove_item(member, book):
+        cart_item = member.cart.items.all().filter(book=book).first()
 
         if cart_item:
-            cart_item.delete()
+            member.cart.items.remove(cart_item)
 
     def __str__(self):
         return self.member.user.username
 
 
+class UserCheckout(models.Model):
+    user = models.OneToOneField(User, null=False, blank=False, on_delete=models.CASCADE)
+    braintree_id = models.CharField(max_length=120, null=True, blank=True)
+    reservation = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user
+
+    @property
+    def get_braintree_id(self):
+        if not self.braintree_id:
+            result = gateway.customer.create({
+                "email": "jen@example.com",
+            })
+
+            if result.is_success:
+                self.braintree_id = result.customer.id
+                self.save()
+
+        return self.braintree_id
+
+    def get_client_token(self):
+        customer_id = self.get_braintree_id
+
+        if customer_id:
+            client_token = gateway.client_token.generate({
+                "customer_id": customer_id
+            })
+
+            return client_token
+        else:
+            return None
+
+
+def update_braintree_id(sender, instance, *args, **kwargs):
+    if not instance.braintree_id:
+        return instance.get_braintree_id
+
+
 class Order(models.Model):
-    cart = models.OneToOneField(Cart, null=True, on_delete=models.PROTECT)
-    confirmation_number = models.IntegerField(blank=False)
+    items = models.ManyToManyField(CartItem, blank=True)
+    confirmation_number = models.CharField(max_length=10, unique=True, blank=False)
     date = models.DateField(default=timezone.now, blank=False)
-    address_used = models.OneToOneField("members.Address",
-                                        related_name="address_used",
-                                        null=True,
-                                        on_delete=models.PROTECT)
+    address_used = models.ForeignKey("members.Address",
+                                     related_name="address_used",
+                                     null=True,
+                                     on_delete=models.SET_NULL)
+    reservation = models.BooleanField(default=False)
+    is_fulfilled = models.BooleanField(default=False)
+
+    @property
+    def price(self):
+        price = 0.0
+        for item in self.items.all():
+            price += float(item.price)
+
+        return price
+
+    def cancel_order(self):
+        user = self.member_set.first().user
+        all_items = ''
+
+        for item in self.items.all():
+            all_items += item.book.name + ', '
+
+        send_mail(
+            'Your order has been cancelled',
+
+            user.username + ',\n\nThe following order has been cancelled:' + '\n'
+            + "Customer: " + user.first_name + ' ' + user.last_name + '\n'
+            + "Order Confirmation Number: " + self.confirmation_number + '\n'
+            + 'Order ID: ' + str(self.id) + '\n'
+            + 'Date Order was Placed: ' + str(self.date)[0:10] + '\n'
+            + 'Address Used: ' + self.address_used.location + '\n'
+            + 'Items ordered: ' + all_items[:-2] + '\n'
+            + 'Total amount: ' + '%.2f' % self.price + '\n\n'
+            + 'If this is an error, please contact support at thedogearbookstore@gmail.com',
+
+            'thedogearbookstore@gmail.com',
+            [user.email],
+            fail_silently=True,
+        )
+
+        for item in self.items.all():
+            item.delete()
+        self.delete()
+
+    def __str__(self):
+        return self.confirmation_number
+
+
+def order_id_generator():
+    order_id = "".join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
+    return order_id
+
+
+def post_save_order_receiver(sender, instance, created, **kwargs):
+    if instance.items.all().first():
+        user = instance.member_set.first().user
+        all_items = ''
+
+        for item in instance.items.all():
+            all_items += item.book.name + ', '
+
+        send_mail(
+            'Your order has been placed',
+
+            user.username + ',\n\nThe following order has been placed:' + '\n\n'
+            + "Customer: " + user.first_name + ' ' + user.last_name + '\n'
+            + "Order Confirmation Number: " + instance.confirmation_number + '\n'
+            + 'Order ID: ' + str(instance.id) + '\n'
+            + 'Date Order was Placed: ' + str(instance.date)[0:10] + '\n'
+            + 'Address Used: ' + instance.address_used.location + '\n'
+            + 'Items ordered: ' + all_items[:-2] + '\n'
+            + 'Total amount: ' + '$%.2f' % instance.price + '\n\n'
+            + 'If this is an error, please contact support at thedogearbookstore@gmail.com',
+
+            'thedogearbookstore@gmail.com',
+            [user.email],
+            fail_silently=True,
+        )
+
+post_save.connect(update_braintree_id, sender=UserCheckout)
+post_save.connect(post_save_order_receiver, sender=Order)

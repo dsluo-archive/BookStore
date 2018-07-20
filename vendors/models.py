123e2d054427db5c77596b2d64fa8237bc1cc7dc
Brian-Burgess <brian@brian-burgess.me>
2018-07-20 17:54:41 -0400

    Added physical address for vendor and added it to order_detail.html

--- vendors/models.py
+++ vendors/models.py
@@ -5,14 +5,13 @@ from django.db import models
 from django.db.models.signals import pre_save
 
 # Create your models here.
-
-
 def generate_code():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
 
 
 class Vendor(models.Model):
     vendor_name = models.CharField(max_length=60, unique=True)
+    physical_address = models.ForeignKey('members.Address', on_delete=models.SET_NULL, null=True, blank=False)
     vendor_passcode = models.CharField(max_length=16, default=generate_code, blank=True)
 
     def __str__(self):

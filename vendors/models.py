77804a551ec3f723a27c89f91c6868d27a75e5e1
Brian-Burgess <brian@brian-burgess.me>
2018-07-20 17:45:34 -0400

    Added a vendor_passcode to Vendor model and a generator for that code

--- vendors/models.py
+++ vendors/models.py
@@ -1,15 +1,20 @@
-from django.db import models
+import random
+import string
 
+from django.db import models
+from django.db.models.signals import pre_save
 
 # Create your models here.
 
 
-def upload_location(instance, filename):
-    return "%s/%s" % (instance.id, filename)
+def generate_code():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
 
 
 class Vendor(models.Model):
-    vendor_name = models.CharField(max_length=60)
+    vendor_name = models.CharField(max_length=60, unique=True)
+    vendor_passcode = models.CharField(max_length=16, default=generate_code, blank=True)
 
     def __str__(self):
         return self.vendor_name
+

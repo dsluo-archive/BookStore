42a7cb164fdc148f917435560424f6474cd9b9f9
Brian-Burgess <brian@brian-burgess.me>
2018-07-20 21:00:23 -0400

    Modified BookForm to allow for Publisher model being saved

--- vendors/forms.py
+++ vendors/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from books.models import Book
+from books.models import Book, Publisher
 from vendors.models import Vendor
 
 
@@ -9,13 +9,13 @@ class MassBookForm(forms.Form):
 
 
 class BookForm(forms.ModelForm):
+
     class Meta:
         model = Book
         fields = [
             "item_picture",
             "name",
             "isbn",
-            "publisher",
             "price",
             "author",
             "publish_date",
@@ -23,6 +23,27 @@ class BookForm(forms.ModelForm):
             "description",
             "subjects",
         ]
+
+    publisher = forms.CharField(max_length=120, required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance.publisher:
+            self.fields['publisher'].initial = self.instance.publisher.name
+
+    def save(self, commit=True):
+        book = super(BookForm, self).save(commit=False)
+        book.publisher, created = Publisher.objects.get_or_create(name__iexact=self.cleaned_data['publisher'])
+
+        if created:
+            book.publisher.name = self.cleaned_data['publisher']
+            book.publisher.save()
+
+        if commit:
+            book.save()
+
+        return book
 
 
 class VendorRegistrationForm(forms.Form):

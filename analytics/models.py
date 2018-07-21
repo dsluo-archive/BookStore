3c4d80e0087bdff2a9bc87738584a571b3a98e7d
Brian-Burgess <brian@brian-burgess.me>
2018-07-20 20:57:11 -0400

    analytics models
    + Made date mandatory for all reports
    + added publisher foreignkey

--- analytics/models.py
+++ analytics/models.py
@@ -1,25 +1,26 @@
 from django.db import models
 from django.utils import timezone
 
-from books.models import Book
+from books.models import Book, Publisher
 # Create your models here.
 from cart.models import Order
 
 
 class EodReport(models.Model):
-    date = models.DateField(default=timezone.now)
+    date = models.DateField(default=timezone.now, blank=False)
     orders = models.ManyToManyField(Order)
 
 
 class LowInvReport(models.Model):
-    date = models.DateField(default=timezone.now)
+    date = models.DateField(default=timezone.now, blank=False)
     low_inventory_books = models.ManyToManyField(Book, blank=True)
 
 
 class BookSalesReport(models.Model):
-    date = models.DateField(default=timezone.now)
+    date = models.DateField(default=timezone.now, blank=False)
     book = models.OneToOneField(Book, on_delete=models.PROTECT, blank=True)
 
 
 class PublisherSalesReport(models.Model):
-    pass
+    date = models.DateField(default=timezone.now, blank=False)
+    publisher = models.OneToOneField(Publisher, on_delete=models.PROTECT, blank=True)

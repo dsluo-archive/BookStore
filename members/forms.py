a26f7ddbb2e581bd766dbe0dc6bbfaf678b2a397
Brian-Burgess <brian@brian-burgess.me>
2018-07-22 14:42:33 -0400

    Register and edit form changes:
    + Made email unique in form
    + Made email unique in edit_account
    + Added initial value to email field in edit_account
    + Made CustomUserEditForm to use different validation

--- members/forms.py
+++ members/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 from members.models import Member
 
@@ -25,7 +26,6 @@ class CustomUserCreationForm(forms.ModelForm):
         model = User
         fields = [
             "username",
-            "email",
             "first_name",
             "last_name",
         ]
@@ -38,6 +38,44 @@ class CustomUserCreationForm(forms.ModelForm):
             user.save()
 
         return user
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+
+        if email and User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Account with this email already exists.')
+        else:
+            return email
+
+
+class CustomUserEditForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+        ]
+
+    def save(self, commit=True):
+        user = super(CustomUserEditForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+
+        if commit:
+            user.save()
+
+        return user
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        username = self.cleaned_data['username']
+
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError('Account with this email already exists.')
+        else:
+            return email
 
 
 class MemberForm(forms.ModelForm):
